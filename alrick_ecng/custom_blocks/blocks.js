128a25f6276aeabde971f59166d85fac051c07d5
alrickBrown <alrickab@gmail.com>
2018-10-24 07:46:53 -0500

    added turn block

--- alrick_ecng/custom_blocks/blocks.js
+++ alrick_ecng/custom_blocks/blocks.js
@@ -2,7 +2,7 @@ Blockly.Blocks['move_person'] = {
     init: function() {
       this.appendDummyInput()
           .appendField("Move")
-          .appendField(new Blockly.FieldDropdown([["left","LEFT"], ["right","RIGHT"]]), "move");
+          .appendField(new Blockly.FieldDropdown([["forward","Forward"], "move");
       this.setInputsInline(false);
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
@@ -12,8 +12,29 @@ Blockly.Blocks['move_person'] = {
     }
   };
 
+  Blockly.Blocks['turn_person'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("Turn")
+          .appendField(new Blockly.FieldDropdown([["left","LEFT"], ["right","RIGHT"]]), "turn");
+      this.setInputsInline(false);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(320);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
   Blockly.JavaScript['move_person'] = function(block) {
     var dropdown_move = block.getFieldValue('move');
+    // TODO: Assemble JavaScript into code variable.
+    var code = dropdown_move+';\n';
+    return code;
+  };
+
+  Blockly.JavaScript['turn_person'] = function(block) {
+    var dropdown_move = block.getFieldValue('turn');
     // TODO: Assemble JavaScript into code variable.
     var code = dropdown_move+';\n';
     return code;
