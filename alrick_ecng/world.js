d178510379942198437de4f571262186916275a6
rick1e <alrickab@gmail.com>
2018-10-24 21:21:37 -0400

    Update world.js

--- alrick_ecng/world.js
+++ alrick_ecng/world.js
@@ -1,7 +1,13 @@
+var start = 0;
+
 function move_js()
 {
-  alert("forward");
-  document.getElementById("00Div").style.background = "red";
+  document.getElementById("00Div").style.background = "white";
+  document.getElementById("01Div").style.background = "white";
+  document.getElementById("02Div").style.background = "white";
+  //alert("forward");
+  document.getElementById("0"+start+"Div").style.background = "red";
+  start++;
 }
 
 function turn_js(val)
